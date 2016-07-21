d78b4a9e27b3400b719fb2e91d4de00fe9bfc11d
Diosnel Velazquez <diosnelvelazquez@gmail.com>
2016-07-21 11:33:36 -0400

    alpha 0.1

--- Calculadora/Calculadora/ViewController.swift
+++ Calculadora/Calculadora/ViewController.swift
@@ -12,14 +12,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //print("hola")
         // Do any additional setup after loading the view, typically from a nib.
+        labelResult.text = "\(result)"
+    }
+    
+    var result:Float = 0
+    
+    var actualNumero:Float = 0
+    
+    var resultadoParcial:Float = 0
+    
+    var actualOperacion:String = "="
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
+    @IBAction func numero(sender: UIButton) {
+
+        actualNumero = actualNumero * 10 + Float(Int(sender.titleLabel!.text!)!)
+        labelResult.text = "\(actualNumero)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operacion(sender: UIButton) {
+        let op = sender.titleLabel?.text
+        
+        if (actualOperacion == "+"){
+            resultadoParcial = resultadoParcial + actualNumero
+            labelResult.text = "\(resultadoParcial)"
+        }
+        else if (actualOperacion == "-"){
+            resultadoParcial = resultadoParcial - actualNumero
+            labelResult.text = "\(resultadoParcial)"
+        }
+        else if (actualOperacion == "x"){
+            resultadoParcial = resultadoParcial * actualNumero
+            labelResult.text = "\(resultadoParcial)"
+        }
+        else if (actualOperacion == "/"){
+            resultadoParcial = resultadoParcial / actualNumero
+            labelResult.text = "\(resultadoParcial)"
+        }
+        
+        if op != "=" && resultadoParcial == 0{
+            resultadoParcial = actualNumero
+        }
+        
+        actualNumero = 0
+        actualOperacion = op!
     }
-
-
+    @IBAction func clear(sender: UIButton) {
+        actualNumero = 0
+        resultadoParcial = 0
+        labelResult.text = "\(actualNumero)"
+        actualOperacion = "="
+    }
+   
 }
 
